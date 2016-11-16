63a24e60f5868f90ccc99718dc8e62bd7d6e005b
nkusibob <nkusibob@hotmail.com>
2016-11-16 21:48:16 +0100

    il n'est tjs pas possible d'update par le libelle

--- WinMDI/CoursGrid.Designer.cs
+++ WinMDI/CoursGrid.Designer.cs
@@ -30,20 +30,21 @@
         {
             this.CoursGrid = new System.Windows.Forms.DataGridView();
             this.SaveCours = new System.Windows.Forms.Button();
+            this.SearchCours = new System.Windows.Forms.TextBox();
             ((System.ComponentModel.ISupportInitialize)(this.CoursGrid)).BeginInit();
             this.SuspendLayout();
             // 
             // CoursGrid
             // 
             this.CoursGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.CoursGrid.Location = new System.Drawing.Point(12, 12);
+            this.CoursGrid.Location = new System.Drawing.Point(12, 54);
             this.CoursGrid.Name = "CoursGrid";
             this.CoursGrid.Size = new System.Drawing.Size(353, 185);
             this.CoursGrid.TabIndex = 0;
             // 
             // SaveCours
             // 
-            this.SaveCours.Location = new System.Drawing.Point(265, 207);
+            this.SaveCours.Location = new System.Drawing.Point(265, 263);
             this.SaveCours.Name = "SaveCours";
             this.SaveCours.Size = new System.Drawing.Size(100, 42);
             this.SaveCours.TabIndex = 1;
@@ -51,17 +52,26 @@
             this.SaveCours.UseVisualStyleBackColor = true;
             this.SaveCours.Click += new System.EventHandler(this.SaveCours_Click);
             // 
+            // SearchCours
+            // 
+            this.SearchCours.Location = new System.Drawing.Point(12, 12);
+            this.SearchCours.Name = "SearchCours";
+            this.SearchCours.Size = new System.Drawing.Size(146, 20);
+            this.SearchCours.TabIndex = 2;
+            // 
             // CoursWinForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(482, 261);
+            this.ClientSize = new System.Drawing.Size(496, 341);
+            this.Controls.Add(this.SearchCours);
             this.Controls.Add(this.SaveCours);
             this.Controls.Add(this.CoursGrid);
             this.Name = "CoursWinForm";
             this.Text = "CoursWinForm";
             ((System.ComponentModel.ISupportInitialize)(this.CoursGrid)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -69,5 +79,6 @@
 
         private System.Windows.Forms.DataGridView CoursGrid;
         private System.Windows.Forms.Button SaveCours;
+        private System.Windows.Forms.TextBox SearchCours;
     }
 }
